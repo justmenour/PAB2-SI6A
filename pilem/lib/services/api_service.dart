b2e8f9de8b382bc5aae5b17d3455ec70bbcd9d5b
justmenour <roidlumban290@gmail.com>
2026-03-03 14:30:47 +0700

    4
    4

---
+++ pilem/lib/services/api_service.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = '3283ae7ba54c153ea13aa08cd78a8d39';
+
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+  
+  // getTrendingMovies
+  // getPopularMovies
+}
